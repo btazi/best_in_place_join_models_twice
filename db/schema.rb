24b05140a13e8fea09a3b5136c4bbe21f3192d77
Badr Tazi <tasibadr@gmail.com>
2014-12-17 15:31:32 +0000

    added event and research booleans to user_countries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217140818) do
+ActiveRecord::Schema.define(version: 20141217152850) do
 
   create_table "countries", force: true do |t|
     t.string "name"
